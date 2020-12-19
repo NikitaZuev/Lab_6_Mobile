fd8cc13292c0a3bbdffe9e18cc7ef289b4a4ec68
Kalpon Gilop <bridgez712@gmail.com>
2020-12-19 16:20:36 +0700

    7.Создайте папку api в вашем проекте, в ней создайте интерфейс FlickrFetch и класс FetchItemTask.

---
+++ app/src/main/java/com/example/a/photogallery/api/FlickrFetch.java
@@ -0,0 +1,14 @@
+package com.example.a.photogallery.api;
+
+import com.example.a.photogallery.model.Example;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FlickrFetch {
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=2146a6386327a0d18dae60b59b1c5320&extras=url_s&format=json&nojsoncallback=1")
+    Call<Example> getRecent();
+    @GET("services/rest/?method=flickr.photos.search&api_key=2146a6386327a0d18dae60b59b1c5320&extras=url_s&format=json&nojsoncallback=1")
+    Call<Example> getSearchPhotos(@Query("text") String keyWord);
+}
